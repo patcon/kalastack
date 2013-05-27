67c7ef3e71892c972b83691de0055ed26c87cf59
Patrick Connolly <patrick@myplanetdigital.com>
2013-05-26 21:25:46 -0400

    Makes the puppet-setup script append code a little more readable.

--- scripts/puppet-setup.sh
+++ scripts/puppet-setup.sh
@@ -12,15 +12,18 @@ if [ ! -f /etc/kalabox/uuid ]; then
   if grep --quiet certname /etc/puppet/puppet.conf; then
     echo
   else
-    echo '[agent]' >> /etc/puppet/puppet.conf
-    echo 'classfile       = $vardir/classes.txt' >> /etc/puppet/puppet.conf
-    echo 'localconfig     = $vardir/localconfig' >> /etc/puppet/puppet.conf
-    echo 'report          = true' >> /etc/puppet/puppet.conf
-    echo 'pluginsync      = true' >> /etc/puppet/puppet.conf
-    echo 'masterport      = 8140' >> /etc/puppet/puppet.conf
-    echo 'environment     = production' >> /etc/puppet/puppet.conf
-    echo 'server          = kalabox.kalamuna.com' >> /etc/puppet/puppet.conf
-    echo 'listen          = false' >> /etc/puppet/puppet.conf
+    # Silently append snippet below to puppetconf
+    cat << 'EOF' | tee -a /etc/puppet/puppet.conf > /dev/null
+[agent]
+classfile       = $vardir/classes.txt
+localconfig     = $vardir/localconfig
+report          = true
+pluginsync      = true
+masterport      = 8140
+environment     = production
+server          = kalabox.kalamuna.com
+listen          = false
+EOF
     service puppet restart -y
   fi
 fi
